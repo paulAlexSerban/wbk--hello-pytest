bafeffea88b53d80db3ce4525ceb0341da72076d
paulAlexSerban <paul.alex.serban@gmail.com>
2022-05-05 16:55:13 +0300

    fix(ci): update

--- release.config.js
+++ release.config.js
@@ -1,23 +1,28 @@
 module.exports = {
   branches: "main",
-  repositoryUrl: "https://github.com/paulAlexSerban/tpl---reverse-proxy-web-server",
+  repositoryUrl:
+    "https://github.com/paulAlexSerban/tpl---reverse-proxy-web-server",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/npm", {
-      npmPublish: false,
-    }],
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
     [
       "@semantic-release/github",
-      { assets: [ {path: "build.zip", label: "Build"} ] }
+      { assets: [{ path: "build.zip", label: "Build" }] },
     ],
+    ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
     [
-      '@semantic-release/changelog',
-      { changelogFile: 'CHANGELOG.md' }
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "docs/**", "package.json"],
+        message:
+          "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
     ],
-    [ '@semantic-release/git',
-      { assets: ['CHANGELOG.md', 'docs/**', 'package.json'],
-        message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}' }
-    ]
   ],
 };
