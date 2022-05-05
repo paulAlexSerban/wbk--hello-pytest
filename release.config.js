9a56397b52d7547464897f0bab5f31e711c4d532
paulAlexSerban <paul.alex.serban@gmail.com>
2022-05-05 15:13:50 +0300

    ci(semantic-release): update release.config.js

--- release.config.js
+++ release.config.js
@@ -4,12 +4,26 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    '@semantic-release/npm',
     [
       "@semantic-release/github",
       {
         assets: [
           {path: "build.zip", label: "Build"}
         ]
+      }
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'docs/**'],
+        message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
     ]
   ],
